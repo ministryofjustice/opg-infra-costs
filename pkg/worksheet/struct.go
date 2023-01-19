87d33fb123d71b1eee0a4d0dac6eb7574cde7c99
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2023-01-19 10:39:18 +0000

    remove setters as those should vary on the type of struct

--- pkg/worksheet/struct.go
+++ pkg/worksheet/struct.go
@@ -13,36 +13,22 @@ type SheetInterface interface {
 	SetActive(b bool) error
 	GetActive() (bool, error)
 
-	SetTableConfiguration(c SheetTableConfigurationInterface) error
 	GetTableConfiguration() (SheetTableConfigurationInterface, error)
-
-	SetFilterConfiguration(c SheetFilterConfigurationInterface) error
 	GetFilterConfiguration() (SheetFilterConfigurationInterface, error)
-
-	SetPaneConfiguration(c SheetPaneConfigurationInterface) error
 	GetPaneConfiguration() (SheetPaneConfigurationInterface, error)
 }
 
 type SheetTableConfigurationInterface interface {
-	SetEnabled(b bool) error
 	GetEnabled() (bool, error)
-
-	SetRange(min string, max string) error
 	GetRange() (string, string, error)
 }
 
 type SheetFilterConfigurationInterface interface {
-	SetEnabled(b bool) error
 	GetEnabled() (bool, error)
-
-	SetRange(min string, max string) error
 	GetRange() (string, string, error)
 }
 
 type SheetPaneConfigurationInterface interface {
-	SetEnabled(b bool) error
 	GetEnabled() (bool, error)
-
-	SetLocation(x int, y int) error
 	GetLocation() (int, int, error)
 }
