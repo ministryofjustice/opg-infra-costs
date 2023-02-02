74f23077c2a2c0d776a721dd98c96af130e299e8
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2023-02-02 10:53:24 +0000

    add cell from def call

--- pkg/report2/column.go
+++ pkg/report2/column.go
@@ -1,13 +1,19 @@
 package report2
 
+// Column is used to define where and how data in cells comes from
+//   - Does not require dataset loading, just config
+//   - SourceColumn maps to the csv row field name
+//   - Display is the value used for this column header
+//   - Forumla can be set so the entire column uses a templated
+//     formula (eg to work out SUM of a row)
 type Column struct {
-	SourceColumn   string
-	Display        string
-	Formula        string
-	NumberFormat   int
-	TransposeRange MonthRange
+	SourceColumn string
+	Display      string
+	Formula      string
+	Definition   *ColumnDefinition
 }
 
+// Key returns the value to use as a key against the csv data row
 func (col *Column) Key() (key string) {
 	if len(col.SourceColumn) > 0 {
 		key = col.SourceColumn
@@ -17,4 +23,38 @@ func (col *Column) Key() (key string) {
 		key = ""
 	}
 	return
+}
+
+// -- New / Create methods
+
+// NewColumnsFromDefinition uses the definition to generate a `[]Column`.
+// For most, this will be singular, but for ColumnDefinition's that have
+// tranpositions to do it will create one for each
+//   - Formula data is carried over to each Column
+//   - Defintiion styling information is not used here, but definitions
+//     are attached for later user
+func NewColumnsFromDefinition(definition ColumnDefinition) (cols []Column) {
+	// if this is a transposed column, we need to generate multiple columns
+	if definition.IsTransposed() {
+		// create a column for each month in the transposed range
+		for _, m := range definition.TransposeMonths.Months() {
+			c := Column{
+				SourceColumn: m,
+				Display:      m,
+				Formula:      definition.Formula,
+				Definition:   &definition,
+			}
+			cols = append(cols, c)
+		}
+	} else {
+		c := Column{
+			SourceColumn: definition.SourceColumn,
+			Display:      definition.Display,
+			Formula:      definition.Formula,
+			Definition:   &definition,
+		}
+		cols = append(cols, c)
+	}
+	return
+
 }
